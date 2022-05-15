68befbbb729b1c6b22b1c7934a244267b8e7bcf4
sam <jsamdan2002@yahoo.de>
2022-05-15 14:19:09 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
+var activePlayer = 1;
+
+// Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+
+// Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var dice = Math.floor(Math.random() * 6) + 1;
+
+{
+  /* <div class="player-score" id="score-0">43</div> */
+}
+// document.querySelector("#score-0").textContent = dice;
+// document.querySelector("#score-1").innerHTML = "<em>Yes!</em>";
+// Програм эхэлхэд бэлтгэе
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+
+document.querySelector(".dice").style.display = "none";
+
+console.log("Шоо: " + dice);
